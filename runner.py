3b995f86759e619e2aa72e92633d7478588e8d3b
Just a Dev <just.a.van.dev@gmail.com>
2026-04-06 21:12:01 -0700

    reorder

--- runner.py
+++ runner.py
@@ -8,14 +8,14 @@ studio_base_urls = {
     "Lagree West": "https://lagreewest.marianatek.com/api/customer/v1/classes?format=json&location=48717,48721,48719,48720&region=48541&page_size=500",
     "Jaybird": "https://jaybird.marianatek.com/api/customer/v1/classes?page_size=500&location=48717,48784&region=48541&page_size=500",
     "Lagree Studio": "https://lagreestudio.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
-    "Ritual": "https://ritualurbanretreat.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
+    "Lagree Pulse": "https://lagreepulse.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
+    "Evolution": "https://evolutionfitness.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
+    "Tunnel": "https://tunnelpilates.marianatek.com/api/customer/v1/classes?page_size=500&location=48718&region=48541",
     "All Method Studio": "https://allstudio.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
+    "Ritual": "https://ritualurbanretreat.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
     "Hustle": "https://hustleup.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
     "Turf": "https://ourturf.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
     "Spin Society": "https://spinsociety-hustle.marianatek.com/api/customer/v1/classes?page_size=500&location=48719&region=48541",
-    "Lagree Pulse": "https://lagreepulse.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
-    "Evolution": "https://evolutionfitness.marianatek.com/api/customer/v1/classes?page_size=500&location=48717&region=48541",
-    "Tunnel": "https://tunnelpilates.marianatek.com/api/customer/v1/classes?page_size=500&location=48718&region=48541"
 }
 
 result = asyncio.run(fetch_studio_data(studio_base_urls, days=45))
