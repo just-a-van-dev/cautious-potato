e7593ea6f281592e21ef04fd79c19b21a36ab93a
Just a Dev <just.a.van.dev@gmail.com>
2026-07-04 02:03:17 -0700

    filter out

--- filter_out.py
+++ filter_out.py
@@ -109,6 +109,7 @@ FILTER_OUT = {
         "yoga | restorative + breath (warm)",
         "yoga | yin + aromatherapy (warm)",
         "yoga | yin + breath (warm)",
+        "yoga | yin + live sound journey ",
 
     ],
     "zenden": [
@@ -134,5 +135,6 @@ FILTER_OUT = {
         "vinyasa flow + sound bath | hot",
         "yin + sound bath",
         "yin + sound bath | warm",
+        "yin + ancient sound bath | warm ",
     ],
 }
